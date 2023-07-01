38497cbaa90ef06e28164045ac67f1ef78b2b216
Reyhaneh <khalili.reyhaneh2001@gmail.com>
2023-07-01 11:24:33 +0330

    Add some attributes in RegisterUserViewModel

--- EShop.Domain/ViewModels/Account/RegisterUserViewModel.cs
+++ EShop.Domain/ViewModels/Account/RegisterUserViewModel.cs
@@ -9,18 +9,21 @@ namespace EShop.Domain.ViewModels.Account
         [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
         [MaxLength(150, ErrorMessage = "{0} نمی تواند بیش از {1} کاراکتر باشد")]
         [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
 
         [Display(Name = "کلمه عبور")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
         [MaxLength(150, ErrorMessage = "{0} نمی تواند بیش از {1} کاراکتر باشد")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Display(Name = "تکرار کلمه عبور")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
         [MaxLength(150, ErrorMessage = "{0} نمی تواند بیش از {1} کاراکتر باشد")]
         [Compare("Password", ErrorMessage = "کلمه های عبور مغایرت دارند")]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
     }
 }
