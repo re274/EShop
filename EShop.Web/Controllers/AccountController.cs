80f6f3785cdbdb2e5721f7e0ac1bb64f5037671f
Reyhaneh <khalili.reyhaneh2001@gmail.com>
2023-07-01 12:38:05 +0330

    Create Register Post method

--- EShop.Web/Controllers/AccountController.cs
+++ EShop.Web/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EShop.Domain.ViewModels.Account;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EShop.Web.Controllers
 {
@@ -7,6 +8,13 @@ namespace EShop.Web.Controllers
         #region register
         [HttpGet("register")]
         public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterUserViewModel register)
         {
             return View();
         }
