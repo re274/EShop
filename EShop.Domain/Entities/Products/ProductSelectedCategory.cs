f52088f92a7f634d017e7dc323037a90107a632f
Reyhaneh <khalili.reyhaneh2001@gmail.com>
2023-08-20 19:07:20 +0330

    Create ProductSelectedCategory interface table and add its properties

---
+++ EShop.Domain/Entities/Products/ProductSelectedCategory.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EShop.Domain.Entities.Products
+{
+    public class ProductSelectedCategory
+    {
+        #region properties
+
+        public int ProductId { get; set; }
+        public int ProductCategoryId { get; set; }
+
+        #endregion
+
+
+        #region relations
+
+        // Product : name of the relationship
+        // [ForeignKey("ProductId")] : با توجه به پراپرتی بالا، خودش میفهمه
+        public Product Product { get; set; }
+        public ProductCategory ProductCategory { get; set; }
+
+        #endregion
+    }
+}
