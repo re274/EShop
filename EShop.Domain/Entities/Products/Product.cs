8855aa8eafbdd34311d41af484838f9d2fc7d09f
Reyhaneh <khalili.reyhaneh2001@gmail.com>
2023-08-20 18:23:34 +0330

    Create Product entity and add its properties

---
+++ EShop.Domain/Entities/Products/Product.cs
@@ -0,0 +1,19 @@
+﻿using EShop.Domain.Entities.Base;
+using System;
+
+namespace EShop.Domain.Entities.Products
+{
+    public class Product : BaseEntity
+    {
+        #region properties
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+        public int Price { get; set; }
+        public bool IsActive { get; set; }
+
+        #endregion
+
+    }
+}
