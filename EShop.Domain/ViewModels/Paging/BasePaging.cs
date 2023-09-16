2ad20cf362987ecbba1248ce9c7b296e2294a830
Reyhaneh <khalili.reyhaneh2001@gmail.com>
2023-09-16 22:57:27 +0330

    Add Paging folder in ViewModels and its attributes

---
+++ EShop.Domain/ViewModels/Paging/BasePaging.cs
@@ -0,0 +1,17 @@
+﻿namespace EShop.Domain.ViewModels.Paging
+{
+    public class BasePaging
+    {
+        public int Page { get; set; }
+
+        public int AllEntitiesCount { get; set; }
+
+        public int AllPagesCount { get; set;}
+
+        public int Take { get; set;}
+
+        public int Skip { get; set; }
+
+        public int HowManyPagesShowAfterBefore { get; set; }
+    }
+}
