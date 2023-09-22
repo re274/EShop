53e5de37ef18f79ceb8d87e787bed3c14a1af7a5
Reyhaneh <khalili.reyhaneh2001@gmail.com>
2023-09-23 00:12:33 +0330

    Create SiteSlider view in Components - Create SiteSliderViewComponent class in ViewComponents

---
+++ EShop.Web/ViewComponents/SiteSliderViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using EShop.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.ViewComponents
+{
+    public class SiteSliderViewComponent : ViewComponent
+    {
+        private readonly ISiteService _siteService;
+        public SiteSliderViewComponent(ISiteService siteService)
+        {
+            _siteService = siteService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("SiteSlider", _siteService.GetAllActiveSliders());
+        }
+
+        //public Task<IViewComponentResult> InvokeAsync()
+        //{
+        //    return Task.FromResult<IViewComponentResult>(View("SiteSlider", _siteService.GetAllActiveSliders));
+        //}
+    }
+}
